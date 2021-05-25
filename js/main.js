7510929047595ee7aecb4abcfc006fdc50313716
ParkDajeong <sobeast980@gmail.com>
2021-05-25 15:56:53 +0900

    home 레이아웃 구현

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+  $(".home-slide").slick({
+    autoplay: false,
+    autoplaySpeed: 4000,
+    arrows: false,
+    dots: true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+  });
+});
