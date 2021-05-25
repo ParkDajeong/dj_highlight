aaa340a92d282b31ecd209dce2bb28c23da94b5a
ParkDajeong <sobeast980@gmail.com>
2021-05-25 17:21:59 +0900

    gnb 레이아웃 구현

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
+function focusMenu(self) {
+  $(".gnb__menu-link").css("opacity", 0.3);
+  $(self).css("opacity", 1);
+}
+
+function focusOutMenu() {
+  $(".gnb__menu-link").css("opacity", 1);
+}
+
 $(function() {
+  // home slider
   $(".home-slide").slick({
     autoplay: false,
     autoplaySpeed: 4000,
@@ -7,4 +17,20 @@ $(function() {
     pauseOnHover: false,
     pauseOnFocus: false,
   });
+
+  // gnb hover,focus event
+  $(".gnb__menu-link").hover(function() {
+    focusMenu($(this));
+  }, function() {
+    focusOutMenu();
+  });
+  
+  $(".gnb__menu-link").focus(function() {
+    focusMenu($(this));
+  });
+
+  $(".gnb__menu-link").blur(function() {
+    focusOutMenu();
+  });
+  
 });
